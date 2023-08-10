ced0a1d83464ee0828dac81371dd88c281c5328f
Fapopo <39032956+Fapopo@users.noreply.github.com>
2023-08-11 00:51:57 +0900

    a

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 git checkout main
 git add -A
 git commit -m "a"
-git push https://github.com/Fapopo/3d
+git push https://github.com/Fapopo/3d main
 start http://localhost:3001
 start https://threed-avaj.onrender.com
 "C:\Program Files\nodejs\node.exe" app.js
